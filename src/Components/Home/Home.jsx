a764550e9be4af738317847a5d55785f6fbee5c4
Gianfranco Gobbi <gianfrancogobbi@outlook.com>
2023-01-27 15:42:25 -0300

    ubicacion

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,15 +4,16 @@ import img from '../Nav/hojas.svg';
 import Delivery from '../Delivery/Delivery';
 import Carta from '../Carta/Carta';
 import Ubicacion from '../Ubicacion/Ubicacion';
+import imgrain from './rain.svg';
 
 function Home() {
   return <div >
     <Nav />
     <div className='img-mock-container'>
       <img className='img-mock' src={img} alt="img-app"></img>
-      <img className='img-mock' src={img} alt="img-app"></img>
-      <img className='img-mock' src={img} alt="img-app"></img>
-      <img className='img-mock' src={img} alt="img-app"></img>
+      <img className='img-rain' src={imgrain} alt="img-app"></img>
+      <img className='img-rain' src={imgrain} alt="img-app"></img>
+      <img className='img-mock subido' src={img} alt="img-app"></img>
     </div>
     <Delivery />
     <Carta />
